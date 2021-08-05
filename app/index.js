69599753537e28f56b7c0bcf0af7d09758a7d408
emquiroga <emiliano.quiroga093@gmail.com>
2021-08-05 12:24:25 -0300

    Finished
    JS validation and responsive layout  done

--- app/index.js
+++ app/index.js
@@ -1,59 +1,67 @@
 const form = document.getElementsByTagName('form')[0];
+const firstName = document.getElementById('first-name');
+const spanFirst = document.getElementById('error-msg-fname');
+const lastName = document.getElementById('last-name');
+const spanLast = document.getElementById('error-msg-lname');
+const email = document.getElementById('email');
+const spanEmail = document.getElementById('error-msg-email');
+const password = document.getElementById('password');
+const spanPass = document.getElementById('error-msg-pwd');
 
-const fnameInput = document.getElementById('first-name');
-const lnameInput = document.getElementById('last-name');
-const emailInput = document.getElementById('email');
-const pwdInput = document.getElementById('password');
-const inputs = [fnameInput, lnameInput, emailInput, pwdInput];
-
-//First name validation
-fnameInput.addEventListener('input', function(e) {
-    if (!fnameInput.validity.valid) {
-        document.getElementById('error-msg-fname').textContent = `First name cannot be empty`;
-        document.getElementById('icon-fname').className = '__icon';
-    } else {
-        document.getElementById('error-msg-fname').textContent = '';
-        document.getElementById('icon-fname').className = '';
-    }
-});
-
-//Last name validation
-lnameInput.addEventListener('input', function(e) {
-    if (!lnameInput.validity.valid) {
-        document.getElementById('error-msg-lname').textContent = 'Last Name cannot be empty';
-        document.getElementById('icon-lname').className = '__icon';
-    } else {
-        document.getElementById('error-msg-lname').textContent = '';
-        document.getElementById('icon-lname').className = '';
-    }
-});
-
-//Email validation
-emailInput.addEventListener('input', function (e) {
-    if(!emailInput.validity.valid) {
-        document.getElementById('error-msg-email').textContent = 'Looks like this is not an email';
-        document.getElementById('icon-email').className = '__icon';
-    } else {
-        document.getElementById('error-msg-email').textContent = '';
-        document.getElementById('icon-email').className = '';
-    }
-});
-
-//Password validation
-pwdInput.addEventListener('input', function (e) { 
-    if(!pwdInput.validity.valid) {
-        document.getElementById('error-msg-pwd').textContent = 'Password cannot be empty'
-        document.getElementById('icon-pwd').className = '__icon'
-    } else {
-        document.getElementById('error-msg-pwd').textContent = ''
-        document.getElementById('icon-pwd').className = ''
-    }
-});
-
+//Adding error classes when submit
 form.addEventListener('submit', function (e) {
-    if (!fnameInput.validity.valid && 
-        !lnameInput.validity.valid &&
-        !emailInput.validity.valid &&
-        !pwdInput.validity.valid)
-        e.preventDefault;
+    if (!firstName.validity.valid) {
+        firstName.classList.add('error');
+        firstName.nextElementSibling.classList.add('__icon');
+        spanFirst.innerHTML = 'First name cannot be empty';
+        e.preventDefault();
+    }
+    if (!lastName.validity.valid) {
+        lastName.classList.add('error');
+        lastName.nextElementSibling.classList.add('__icon');
+        spanLast.innerHTML = 'Last Name cannot be empty';
+        e.preventDefault();
+    }
+    if (!email.validity.valid) {
+        email.classList.add('error');
+        email.nextElementSibling.classList.add('__icon');
+        spanEmail.innerHTML = 'Looks like this is not an email';
+        e.preventDefault();
+    }
+    if (!password.validity.valid) {
+        password.classList.add('error');
+        password.nextElementSibling.classList.add('__icon');
+        spanPass.innerHTML = 'Password cannot be empty';
+        e.preventDefault();
+    }
+});
+
+//Checking validity when input changes its value, after submit
+firstName.addEventListener('input', function () {
+    if (firstName.validity.valid) {
+        firstName.classList.remove('error')//Removes the class error if valid
+        firstName.nextElementSibling.classList.remove('__icon'); //Removes alert icon
+        spanFirst.innerHTML = ''; //Clean the span content
+    }
+});
+lastName.addEventListener('input', function () {
+    if (lastName.validity.valid) {
+        lastName.classList.remove('error')//Removes the class error if valid
+        lastName.nextElementSibling.classList.remove('__icon'); //Removes alert icon
+        spanLast.innerHTML = ''; //Clean the span content
+    }
+});
+email.addEventListener('input', function () {
+    if (email.validity.valid) {
+        email.classList.remove('error')//Removes the class error if valid
+        email.nextElementSibling.classList.remove('__icon'); //Removes alert icon
+        spanEmail.innerHTML = ''; //Clean the span content
+    }
+});
+password.addEventListener('input', function () {
+    if (password.validity.valid) {
+        password.classList.remove('error')//Removes the class error if valid
+        password.nextElementSibling.classList.remove('__icon'); //Removes alert icon
+        spanPass.innerHTML = ''; //Clean the span content
+    }
 });
